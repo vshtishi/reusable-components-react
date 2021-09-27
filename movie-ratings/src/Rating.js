e58924e19b80052d31a698757f17d450ef14d139
Vjola <vjolashtishi@gmail.com>
2021-09-27 10:07:37 +0200

    Movie Ratings Initial Components

---
+++ movie-ratings/src/Rating.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
+const Rating = () => {
+  return <h2>Rating component</h2>;
+};
+
+export default Rating;
