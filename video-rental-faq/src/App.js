178d557c4f1b298e0c7a84d413ad7bf3a7120b2a
Vjola <vjolashtishi@gmail.com>
2021-09-28 14:11:04 +0200

    Built Accordion FAQ List

--- video-rental-faq/src/App.js
+++ video-rental-faq/src/App.js
@@ -2,7 +2,20 @@ import React, { useState } from "react";
 import data from "./data";
 import SingleQuestion from "./Question";
 function App() {
-  return <h2>FAQ Page</h2>;
+  const [questions, setQuestions] = useState(data);
+
+  return (
+    <main>
+      <div className="container">
+        <h3>Video Rental FAQ</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return <SingleQuestion key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- video-rental-faq/src/data.js
+++ video-rental-faq/src/data.js
@@ -1,3 +1,29 @@
-const questions = []
+const questions = [
+  {
+    id: 1,
+    title: "How can I find out if a movie is available at Video Rental?",
+    info: "To inquire if Video Rental carries a specific title, please call your local store. Find your closest store by using our store locator. If you would like to buy a title to own, you may order online  or ask any customer service representative to place an order in-store.",
+  },
+  {
+    id: 2,
+    title: "May children under age 17 rent R-rated films or M-rated games?",
+    info: "Only with parental consent may children rent R-rated movies or M-rated games. Please let a customer representative know if you would like to restrict rentals on your account -- We will not rent R-rated movies or M-rated games to those 17 or under without parental consent noted on the account.",
+  },
+  {
+    id: 3,
+    title: "Can I reserve a movie at my local Video Rental?",
+    info: "If you call your local Video Rental and we have a rental item in stock you would like to reserve, we will reserve it at the counter in your name for up to 1/2 hour to give you an opportunity to pick it up.",
+  },
+  {
+    id: 4,
+    title: "Are gift cards good at all stores?",
+    info: "es, if you purchase a gift card from a Video Rental store, it is good at any of our locations or online.",
+  },
+  {
+    id: 5,
+    title: "How do I become a member at Video Rental?",
+    info: "It’s free and easy to have a rental account with any Video Rental. All you need is a driver’s license or state ID and a phone number that we verify is yours. Signup only takes a few minutes in the store.",
+  },
+];
 
-export default questions
+export default questions;
