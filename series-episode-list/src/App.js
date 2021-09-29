0fcb21931c9bb30d34f6a39fdb7257d7992928d9
Vjola <vjolashtishi@gmail.com>
2021-09-29 16:15:38 +0200

    Episodes Components Basic Setup

--- series-episode-list/src/App.js
+++ series-episode-list/src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
+import data from "./data";
 
 function App() {
+  const [episodes, setEpisodes] = useState(data);
+  const [value, setValue] = useState(0);
+
+  const { order, date, info, title } = episodes[value];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>Mr.Robot Episodes</h2>
+        <div className="underline"></div>
+      </div>
+
+      <div className="episodes-center">
+        {/* */}
+        {/* */}
+
+        <article className="episode-info">
+          <h3>{title}</h3>
+          <h4>Episode {order}</h4>
+          <p className="episode-date">Released: {date}</p>
+          
+          <div className="episode-desc">
+            <FaAngleDoubleRight className="episode-icon" />
+            <p>Directed By: {info.directed}</p>
+          </div>
+
+          <div className="episode-desc">
+            <FaAngleDoubleRight className="episode-icon" />
+            <p>Viewers: {info.viewers}</p>
+          </div>
+
+          <div className="episode-desc">
+            <FaAngleDoubleRight className="episode-icon" />
+            <p>Synopsis: {info.desc}</p>
+          </div>
+        </article>
+      </div>
+    </section>
   );
 }
 
---
+++ series-episode-list/src/data.js
@@ -0,0 +1,37 @@
+const data = [
+  {
+    id: "recAGJfiU4CeaV0HL",
+    order: 1,
+    title: "Hello Friend",
+    date: "June 24, 2015",
+    info: {
+      directed: "Niels Arden Oplev",
+      viewers: "1.75",
+      desc: "Socially inept Elliot Alderson works at cyber security company Allsafe. At night, he hacks social media, personal information, and bank records, including those of his co-workers, therapist, and drug dealer. Elliot stops a DDoS attack against Allsafe's biggest client, E Corp. He identifies a file labeled fsociety00.dat and a text file asking him not to destroy malware he finds hidden on E Corp's server. Mr. Robot, the mysterious leader of the hacker group fsociety, contacts Elliot on the subway. Fsociety invites Elliot to join them in starting a digital revolution; they plan to delete all debt records held by E Corp. He is intrigued and eventually agrees. As his first collaborative act with fsociety, Elliot provides the FBI with an encrypted file falsely implicating Terry Colby, E Corp's own chief technology officer (CTO) as the orchestrator of the attack.",
+    },
+  },
+  {
+    id: "recIL6mJNfWObonls",
+    order: 2,
+    title: "Ones and Zeores",
+    date: "July 1, 2015",
+    info: {
+      directed: "Sam Esmail",
+      viewers: "1.73",
+      desc: "Elliot declines a job offer from Tyrell Wellick, interim CTO of E Corp. Elliot, increasingly paranoid, uses more morphine than normal. Darlene visits him at home to bring him to fsociety, surprising him. Mr. Robot and fsociety want Elliot to destroy an E Corp offline backup facility called Steel Mountain by hacking an adjacent gas plant and causing an explosion. Elliot backs out because he does not want to risk lives. Shayla is Elliot's drug dealer and friend. When Elliot notices Fernando Vera, Shayla's supplier, abusing her, Elliot collects evidence against Fernando and turns him in to the police, despite Shayla's explicit instructions not to. Meanwhile, Cisco, an unknown associate of the Chinese hacker group The Dark Army, hacks Ollie's home computer with a CD. Elliot and Mr. Robot discuss Elliot's falling out with his father: Elliot told his mother of his father's terminal illness, despite promising his father he would keep the illness secret. Mr. Robot pushes Elliot off a railing onto the rock-strewn beach below as punishment for not keeping his father's secret.",
+    },
+  },
+  {
+    id: "rec61x18GVY99hQq5",
+    order: 3,
+    title: "Debug",
+    date: "July 8, 2015",
+    info: {
+      directed: "Jim McKay",
+      viewers: "1.6",
+      desc: "Tyrell is enraged that Scott Knowles was chosen for CTO. To let off steam, Tyrell pays a homeless man to allow him to beat him. Cisco demands that Ollie infect Allsafe with the same CD. Elliot believes he is done with fsociety. Tyrell seduces his boss's male secretary and installs software on his phone. Attempting to be more normal, Elliot asks Shayla to accompany him to a party, she accepts and they become romantically involved. During the party, fsociety leaks E Corp's emails that incriminate Colby for covering up a toxic waste leak that caused leukemia in Elliot's father and Angela's mother. Tyrell ties up his pregnant wife and ball-gags her upon her request. Ollie confesses both his infidelity and Cisco's demands to Angela. Fearing identity theft, she wants him to infect Allsafe with the CD. Elliot returns to fsociety. The CEO of Allsafe, Gideon Goddard becomes suspicious of Elliot's story regarding the E Corp attack, and begins to investigate.",
+    },
+  },
+];
+
+export default data;
