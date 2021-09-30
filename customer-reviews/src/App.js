913f847f567514d2644d044e441cc15d97eeb7ea
Vjola <vjolashtishi@gmail.com>
2021-09-30 10:51:29 +0200

    Slider Basic Setup

--- customer-reviews/src/App.js
+++ customer-reviews/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
+import { FaQuoteRight } from "react-icons/fa";
+import data from "./data";
 function App() {
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2>Customer Reviews</h2>
+        <div className="section-center">
+          {people.map((person, personIndex) => {
+            const { id, image, name, title, quote } = person;
+            let position = 'nextSlide'
+            if(personIndex === index) {
+              position='activeSlide'
+            }
+            if(personIndex === index - 1 || (index === 0 && personIndex == people.length - 1)){
+              position = 'lastIndex'
+            }
+            return (
+              <article key={id} className={position}>
+                <img src={image} alt={name} className="person-img" />
+                <h4>{name}</h4>
+                <p className="title">{title}</p>
+                <p className="text">{quote}</p>
+                <FaQuoteRight className="icon" />
+                <button className="prev">
+                  <FiChevronLeft />
+                </button>
+                <button className="next">
+                  <FiChevronRight />
+                </button>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </section>
   );
 }
 
