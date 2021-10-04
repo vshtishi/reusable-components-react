80d954ef9f84d0deb47607dc539bfe3675bd486a
Vjola <vjolashtishi@gmail.com>
2021-10-04 16:23:30 +0200

    Added Submit Form

--- color-palette/src/App.js
+++ color-palette/src/App.js
@@ -4,7 +4,45 @@ import SingleColor from "./SingleColor";
 import Values from "values.js";
 
 function App() {
-  return <h2>Color Palette</h2>;
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors)
+    } catch (error) {
+      setError(true);
+    }
+  };
+  return (
+    <>
+      <section className="container">
+        <h3>Color Palette</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            placeholder=" #693b58"
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            Submit
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+       {
+         list.map((color, index) => {
+            return <SingleColor key={index} {...color} index={index} />
+         })
+       }
+      </section>
+    </>
+  );
 }
 
 export default App;
