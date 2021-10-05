b2d2304f9089ec16a603326d909e9b40eb6c72a3
Vjola <vjolashtishi@gmail.com>
2021-10-05 09:23:52 +0200

    Color Palette Final Fixes

--- color-palette/src/App.js
+++ color-palette/src/App.js
@@ -6,13 +6,14 @@ import Values from "values.js";
 function App() {
   const [color, setColor] = useState("");
   const [error, setError] = useState(false);
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(new Values("#693b58").all(10));
 
   const handleSubmit = (e) => {
     e.preventDefault();
     try {
       let colors = new Values(color).all(10);
-      setList(colors)
+      setList(colors);
+      setError(false);
     } catch (error) {
       setError(true);
     }
@@ -35,11 +36,9 @@ function App() {
         </form>
       </section>
       <section className="colors">
-       {
-         list.map((color, index) => {
-            return <SingleColor key={index} {...color} index={index} />
-         })
-       }
+        {list.map((color, index) => {
+          return <SingleColor key={index} {...color} index={index} />;
+        })}
       </section>
     </>
   );
