8a7d1132eb8a192cec7e48d166ae5796d7b88c4b
Vjola <vjolashtishi@gmail.com>
2021-10-05 15:59:21 +0200

    Added Alerts

--- recipe-form/src/App.js
+++ recipe-form/src/App.js
@@ -4,19 +4,41 @@ import Alert from "./Alert";
 
 function App() {
   const [name, setName] = useState("");
-  const [quantity, setQuantity] = useState(0);
+  const [quantity, setQuantity] = useState("");
 
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editID, setEditID] = useState(null);
   const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
   const handleSubmit = (e) => {
+    if (!name) {
+      showAlert(true, "danger", "Please enter ingredient");
+    } else if (!quantity) {
+      showAlert(true, "danger", "Please enter quantity");
+    } else if (name && quantity && isEditing) {
+      //deal with edit
+    } else {
+      //show alert
+      const newIngredient = {
+        id: new Date().getTime().toString(),
+        title: name,
+        quantity,
+      };
+      setList([...list, newIngredient]);
+      setName("");
+      setQuantity("");
+    }
     e.preventDefault();
   };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+
   return (
     <section className="section-center">
       <form className="recipe-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>Recipe</h3>
         <div className="form-control">
           <input
@@ -27,7 +49,7 @@ function App() {
             onChange={(e) => setName(e.target.value)}
           />
           <input
-            type="number"
+            type="text"
             className="recipe"
             placeholder="e.g 1"
             value={quantity}
@@ -39,8 +61,10 @@ function App() {
         </div>
       </form>
       <div className="recipe-container">
-        <Recipe />
-        <button className="clear-btn">clear ingredients</button>
+        <Recipe items={list} />
+        {list.length > 0 && (
+          <button className="clear-btn">clear ingredients</button>
+        )}
       </div>
     </section>
   );
--- recipe-form/src/Recipe.js
+++ recipe-form/src/Recipe.js
@@ -1,7 +1,27 @@
 import React from "react";
 import { FaEdit, FaTrash } from "react-icons/fa";
-const Recipe = () => {
-  return <h2>Recipe component</h2>;
+
+const Recipe = ({ items }) => {
+  return (
+    <div className="recipe-list">
+      {items.map((item) => {
+        const { id, title, quantity } = item;
+        return (
+          <article key={id} className="recipe-item">
+            <p className="title">{quantity + " " + title}</p>
+            <div className="btn-container">
+              <button type="button" className="edit-btn">
+                <FaEdit />
+              </button>
+              <button type="button" className="delete-btn">
+                <FaTrash />
+              </button>
+            </div>
+          </article>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Recipe;
