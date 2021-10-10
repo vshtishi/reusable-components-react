c1103ce991d08f3610716180ae78e19af0eab041
Ubuntu19.10 <vjolashtishi@gmail.com>
2021-10-10 20:21:03 +0200

    Fixed Navbar and Sidebar

--- video-rental-landing/src/Hero.js
+++ video-rental-landing/src/Hero.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import phoneImg from './images/phone.svg'
-import {useGlobalContext} from './context'
+import { useGlobalContext } from './context'
 const Hero = () => {
-  const data = useGlobalContext()
-  console.log(data)
-  return <h2>hero component</h2>
+  const { closeSubmenu } = useGlobalContext()
+
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Super efficient Video Rental Service
+          </h1>
+          <p>Join thousands of happy customers and rent out your favorite video
+            from our extensive rental collection. Purchases are done in a matter of seconds!
+          </p>
+          <button className="btn">
+            Start now
+          </button>
+        </article>
+        <article className="hero-images">
+          <img src={phoneImg} className="phone-img" alt="phone" />
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Hero
