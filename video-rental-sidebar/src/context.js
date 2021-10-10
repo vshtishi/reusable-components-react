7046ad690002704673eee119f742b6f89db74d7f
Ubuntu19.10 <vjolashtishi@gmail.com>
2021-10-10 13:15:57 +0200

    Sidebar Setup Context

--- video-rental-sidebar/src/context.js
+++ video-rental-sidebar/src/context.js
@@ -1,1 +1,21 @@
-import React, { useState, useContext } from 'react'
+
+import React, { useState, useContext } from 'react';
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+
+    return (
+        <AppContext.Provider
+            value="test"
+        >
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export const useGlobalContext = () => {
+    return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
