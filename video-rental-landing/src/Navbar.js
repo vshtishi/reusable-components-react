61ad58bad099d040bb63c263ba6dbe045bd73811
Vjola <vjolashtishi@gmail.com>
2021-10-11 10:47:31 +0200

    Added Dynamic Submenus

--- video-rental-landing/src/Navbar.js
+++ video-rental-landing/src/Navbar.js
@@ -1,38 +1,48 @@
-import React from 'react'
-import logo from './images/logo.svg'
-import { FaBars } from 'react-icons/fa'
-import { useGlobalContext } from './context'
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext()
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  const displaySubmenu = (e) => {
+    const page = e.target.textContent;
+    const tempBtn = e.target.getBoundingClientRect();
+    const center = (tempBtn.left + tempBtn.right) / 2;
+    const bottom = tempBtn.bottom - 3;
+    openSubmenu(page, {center, bottom});
+  };
   return (
     <nav className="nav">
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="video rental" className="nav-logo" />
-          <button className="btn toggle-btn" onClick={openSidebar}><FaBars /></button>
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
         </div>
         <ul className="nav-links">
           <li>
-            <button className="link-btn">
+            <button className="link-btn" onMouseOver={displaySubmenu}>
               products
-           </button>
+            </button>
           </li>
           <li>
-            <button className="link-btn">
+            <button className="link-btn" onMouseOver={displaySubmenu}>
               videos
-              </button>
+            </button>
           </li>
-          <li> <button className="link-btn">
-            company
-           </button>
+          <li>
+            {" "}
+            <button className="link-btn" onMouseOver={displaySubmenu}>
+              company
+            </button>
           </li>
-
         </ul>
         <button className="btn signin-btn">Sign in</button>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- video-rental-landing/src/context.js
+++ video-rental-landing/src/context.js
@@ -5,7 +5,8 @@ const AppContext = React.createContext()
 
 export const AppProvider = ({ children }) => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false)
-    const [isSubmenuOpen, setIsSubmenuOpen] = useState(true)
+    const [isSubmenuOpen, setIsSubmenuOpen] = useState(false)
+    const [location, setLocation] = useState({})
 
     const openSidebar = () => {
         setIsSidebarOpen(true)
@@ -15,7 +16,8 @@ export const AppProvider = ({ children }) => {
         setIsSidebarOpen(false)
     }
 
-    const openSubmenu = () => {
+    const openSubmenu = (text, coordinates) => {
+        setLocation(coordinates)
         setIsSubmenuOpen(true)
     }
 
@@ -25,7 +27,7 @@ export const AppProvider = ({ children }) => {
 
     return <AppContext.Provider value={{
         isSubmenuOpen, isSidebarOpen, openSubmenu,
-        openSidebar, closeSubmenu, closeSidebar
+        openSidebar, closeSubmenu, closeSidebar, location
     }}>
         {children}
     </AppContext.Provider>
