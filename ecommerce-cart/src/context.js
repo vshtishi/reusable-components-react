81657ae867aaad3b1dcd20c49df308a92eec312a
Vjola <vjolashtishi@gmail.com>
2021-10-11 15:59:32 +0200

    Added Clear Cart Functionality

--- ecommerce-cart/src/context.js
+++ ecommerce-cart/src/context.js
@@ -2,16 +2,26 @@ import React, { useState, useContext, useReducer, useEffect } from "react";
 import cartItems from "./data";
 import reducer from "./reducer";
 
-
+const initialState = {
+  loading: false,
+  cart: cartItems,
+  total: 0,
+  amount: 0,
+};
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [cart, setCart] = useState(cartItems);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
 
   return (
     <AppContext.Provider
       value={{
-        cart,
+        ...state,
+        clearCart
       }}
     >
       {children}
