36c482526dd1e336d9a17d0debcea3772321cdf9
Vjola <vjolashtishi@gmail.com>
2021-10-11 11:57:26 +0200

    Cart Basic Components Setup

---
+++ ecommerce-cart/src/data.js
@@ -0,0 +1,23 @@
+export default [
+  {
+    id: 1,
+    title: "The Shawshank Redemption",
+    price: 9.99,
+    img: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX67_CR0,0,67,98_AL_.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title: "The Godfather: Part II",
+    price: 9.99,
+    img: "https://m.media-amazon.com/images/M/MV5BMWMwMGQzZTItY2JlNC00OWZiLWIyMDctNDk2ZDQ2YjRjMWQ0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_UY98_CR1,0,67,98_AL_.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Se7en",
+    price: 9.99,
+    img: "https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX67_CR0,0,67,98_AL_.jpg",
+    amount: 1,
+  },
+];
