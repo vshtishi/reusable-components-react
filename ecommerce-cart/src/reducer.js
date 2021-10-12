e69d1aa2f5c4dbf4eb4363d75e058b2334289cb2
Vjola <vjolashtishi@gmail.com>
2021-10-12 09:16:20 +0200

    Decreasing Amount of Items in Cart

--- ecommerce-cart/src/reducer.js
+++ ecommerce-cart/src/reducer.js
@@ -13,12 +13,21 @@ const reducer = (state, action) => {
       if (cartItem.id === action.payload) {
         return { ...cartItem, amount: cartItem.amount + 1 };
       }
-      return cartItem
+      return cartItem;
     });
     return { ...state, cart: tempCart };
   }
-  
+
   if (action.type === "DECREASE") {
+    let tempCart = state.cart
+      .map((cartItem) => {
+        if (cartItem.id === action.payload) {
+          return { ...cartItem, amount: cartItem.amount - 1 };
+        }
+        return cartItem;
+      })
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
   }
 
   return state;
