60c2fc5b5d1e493c582b8bc77652bff5bce50b3c
Vjola <vjolashtishi@gmail.com>
2021-10-13 11:07:56 +0200

    Navbar & Error page setup

--- products-page/src/pages/About.js
+++ products-page/src/pages/About.js
@@ -2,9 +2,15 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className="section about-section">
+      <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Consequatur
+        dignissimos cumque odit facere sit officiis modi. Harum quos, laboriosam
+        provident quaerat voluptas facilis, corrupti rem hic facere neque fugit
+        magnam itaque mollitia omnis labore incidunt!
+      </p>
+    </section>
   );
 };
 
