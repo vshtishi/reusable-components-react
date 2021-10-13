fa6d7c3e8834349f1e9d3d9b2ade1895bb0cfe36
Vjola <vjolashtishi@gmail.com>
2021-10-13 15:42:26 +0200

    Made Search Form Functional

--- products-page/src/components/SearchForm.js
+++ products-page/src/components/SearchForm.js
@@ -2,11 +2,31 @@ import React from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  const { setSearchTerm } = useGlobalContext()
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+
+  React.useEffect(() => { searchValue.current.focus()});
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+  }
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search" onSubmit={handleSubmit}>
+      <form className="search-form">
+        <div className="form-control">
+          <label htmlFor="name">Search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
