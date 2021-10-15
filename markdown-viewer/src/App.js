885417137ce22dccb29288d6ab422db88f46d793
Vjola <vjolashtishi@gmail.com>
2021-10-15 14:12:21 +0200

    Final fixes

--- markdown-viewer/src/App.js
+++ markdown-viewer/src/App.js
@@ -2,7 +2,25 @@ import React, { useState } from "react";
 import ReactMarkdown from "react-markdown";
 
 function App() {
-  return <h2>Test</h2>;
+  const [markdown, setMarkdown] = useState("## markdown viewer");
+  return (
+    <main>
+      <section className="markdown">
+        <textarea
+          className="input"
+          value={markdown}
+          onChange={(e) => setMarkdown(e.target.value)}
+          name=""
+          id=""
+          cols="30"
+          rows="10"
+        ></textarea>
+        <article className="result">
+          <ReactMarkdown>{markdown}</ReactMarkdown>
+        </article>
+      </section>
+    </main>
+  );
 }
 
 export default App;
