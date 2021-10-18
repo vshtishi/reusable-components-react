fac920aca2a6a200292ee813948f1c913f6aa387
Vjola <vjolashtishi@gmail.com>
2021-10-18 14:03:43 +0200

    Profile final fixes

--- user-profile-sample/src/App.js
+++ user-profile-sample/src/App.js
@@ -53,7 +53,12 @@ function App() {
   }, []);
 
   const handleValue = (e) => {
-    console.log(e.target);
+    if (e.target.classList.contains("icon")) {
+      const newValue = e.target.dataset.label;
+      setTitle(newValue)
+      setValue(person[newValue])
+       
+    }
   };
   return (
     <main>
