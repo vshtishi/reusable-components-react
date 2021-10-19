d44b8f19eff93ab17898115eb9282b510bd912b4
Vjola <vjolashtishi@gmail.com>
2021-10-19 15:06:27 +0200

    Setup pagination buttons

--- pagination-sample/src/App.js
+++ pagination-sample/src/App.js
@@ -3,7 +3,20 @@ import { useFetch } from "./useFetch";
 import Follower from "./Follower";
 function App() {
   const { loading, data } = useFetch();
-  console.log(data)
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
+  useEffect(() => {
+    if (!loading) {
+      setFollowers(data[page]);
+    } else {
+      return;
+    }
+  }, [loading, page]);
+
+  const handlePage = (index) => {
+    setPage(index);
+  };
   return (
     <main>
       <div className="section-title">
@@ -12,10 +25,25 @@ function App() {
       </div>
       <div className="followers">
         <div className="container">
-          {data.map((follower) => {
-            return <Follower key="follower.id" {...follower} />
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
           })}
         </div>
+        {!loading && (
+          <div className="btn-container">
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? "active-btn" : ""}`}
+                  onClick={() => handlePage(index)}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+          </div>
+        )}
       </div>
     </main>
   );
