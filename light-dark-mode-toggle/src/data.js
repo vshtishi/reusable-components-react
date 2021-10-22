38702fdbdafb487e8f34715cfec03d272dbb3246
Vjola <vjolashtishi@gmail.com>
2021-10-22 12:20:51 +0200

    Basic components setup

---
+++ light-dark-mode-toggle/src/data.js
@@ -0,0 +1,34 @@
+const articles = [
+  {
+    id: 1,
+    title:
+      "In the 'fishbowl' of Brunswick, Georgia, would-be jurors worry about privacy",
+    date: new Date(2021, 10, 4),
+    length: 11,
+    snippet: `Some prospective jurors in the murder trial of the 3 men charged with killing Ahmaud Arbery are already expressing concerns about remaining anonymous.`,
+  },
+  {
+    id: 2,
+    title:
+      "Colin Powell was among the last of a rare breed: A moderate republican",
+    date: new Date(2021, 10, 2),
+    length: 5,
+    snippet: `Completed General Powell's estrangement from the GOP, a distancing that began after Powell declined to run for president in 1996.`,
+  },
+  {
+    id: 3,
+    title: "Justice Thomas hasn't changed in 30 years. The supreme court has.",
+    date: new Date(2020, 7, 11),
+    length: 5,
+    snippet: `With abortion and guns on the docket, Supreme Court Justice Clarence Thomas has an opportunity to wield the influence that comes with his seniority.`,
+  },
+  {
+    id: 4,
+    title: "Supply chain stress leads to overcrowded ports",
+    date: new Date(2020, 5, 4),
+    length: 5,
+    snippet: `The nation's busiest port complex in Los Angeles and Long Beach is overcrowded. The bottleneck is part of a  backup across the global supply chain.`,
+  },
+];
+
+export default articles;
