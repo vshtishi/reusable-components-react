47d75e1d1a67f9df9256511c80ccb84d6f054d16
Vjola <vjolashtishi@gmail.com>
2021-10-22 12:35:05 +0200

    Displaying data

--- light-dark-mode-toggle/src/App.js
+++ light-dark-mode-toggle/src/App.js
@@ -3,7 +3,21 @@ import data from "./data";
 import Article from "./Article";
 
 function App() {
-  return <h2>Test</h2>;
+  return (
+    <main>
+      <nav>
+        <div className="nav-center">
+          <h1>Test</h1>
+          <button className="btn">toggle</button>
+        </div>
+      </nav>
+      <section className="articles">
+        {data.map((item) => {
+          return <Article key={item.id} {...item} />;
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default App;
