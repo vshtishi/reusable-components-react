07f13efaa47f5c7f9f90df1e98871647f79798a9
Vjola <vjolashtishi@gmail.com>
2021-10-26 16:33:57 +0200

    Setting up context

--- video-rental-search/src/Movies.js
+++ video-rental-search/src/Movies.js
@@ -5,7 +5,8 @@ const url =
   "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const Movies = () => {
-  return <h2>movies component</h2>;
+  const { movies, isLoading } = useGlobalContext();
+  return 
 };
 
 export default Movies;
--- video-rental-search/src/context.js
+++ video-rental-search/src/context.js
@@ -1,11 +1,40 @@
 import React, { useState, useContext, useEffect } from "react";
 
-
 export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`;
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="hello">{children}</AppContext.Provider>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState({ show: false, msg: "" });
+  const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("revolutionaryt");
+
+  const fetchMovies = async (url) => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+
+      if (data.Response === "True") {
+        setMovies(data.Search);
+        setError({ show: false, msg: "" });
+      } else {
+        setError({ show: true, msg: data.Error });
+      }
+      setIsLoading(false);
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchMovies(`${API_ENDPOINT}&s=${query}`);
+  }, []);
+  return (
+    <AppContext.Provider value={{ isLoading, error, movies, query, setQuery }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 export const useGlobalContext = () => {
