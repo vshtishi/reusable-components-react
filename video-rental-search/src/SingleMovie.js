b77556cfc4f75f0fe25dbaa6529f8432fdb06fb0
Vjola <vjolashtishi@gmail.com>
2021-10-28 11:14:09 +0200

    Data fetching refactor

--- video-rental-search/src/SingleMovie.js
+++ video-rental-search/src/SingleMovie.js
@@ -1,30 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 import { API_ENDPOINT } from "./context";
+import useFetch from "./useFetch";
 const url =
   "https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
 
 const SingleMovie = () => {
   const { id } = useParams();
-  const [movie, setMovie] = useState();
-  const [isLoading, setLoading] = useState(true);
-  const [error, setError] = useState({ show: false, msg: "" });
-
-  const fetchMovie = async (url) => {
-    const response = await fetch(url);
-    const data = await response.json();
-    if (data.Response === "False") {
-      setError({ show: true, msg: data.Error });
-      setLoading(false);
-    } else {
-      setMovie(data);
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    fetchMovie(`${API_ENDPOINT}&i=${id}`);
-  }, [id]);
+  const { data: movie, isLoading, error } = useFetch(`&i=${id}`);
 
   if (isLoading) {
     return <div className="loading"></div>;
