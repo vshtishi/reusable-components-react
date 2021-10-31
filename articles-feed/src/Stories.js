1805a1ec5e332028d1652780f67168099ef64737
Ubuntu19.10 <vjolashtishi@gmail.com>
2021-10-31 17:28:16 +0100

    Displayed article data

--- articles-feed/src/Stories.js
+++ articles-feed/src/Stories.js
@@ -3,12 +3,30 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Stories = () => {
-    const {isLoading} = useGlobalContext()
+  const { isLoading, hits } = useGlobalContext()
 
-    if(isLoading){
-        return <div className="loading"></div>
-    }
-  return <h2>stories component</h2>;
+  if (isLoading) {
+    return <div className="loading"></div>
+  }
+  return (
+    <section className="stories">
+      {hits.map((story) => {
+        const { objectID, title, num_comments, url, points, author } = story
+        return <article key={objectID} className="story">
+          <h4 className="title">{title}</h4>
+          <p className="info">
+            {points}  points by <span>{author} | </span>
+           {num_comments}{' '}
+           comments
+          </p>
+          <a href={url} className="read-link" target="_blank" rel="noopener noreferrer">
+            read more
+          </a>
+          <button className="remove-btn">remove</button>
+        </article>
+      })}
+    </section>
+  )
 };
 
 export default Stories;
