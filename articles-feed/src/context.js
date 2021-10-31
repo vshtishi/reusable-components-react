1f185b9e9e360245664f187d62176e39f5de7db4
Ubuntu19.10 <vjolashtishi@gmail.com>
2021-10-31 17:07:46 +0100

    Article Data fetching

--- articles-feed/src/context.js
+++ articles-feed/src/context.js
@@ -11,7 +11,13 @@ import reducer from "./reducer";
 
 const API_ENDPOINT = "https://hn.algolia.com/api/v1/search?";
 
-const initialState = { isLoading: true };
+const initialState = {
+  isLoading: true,
+  hits: [],
+  query: 'react',
+  page: 0,
+  nbPages: 0
+};
 
 const AppContext = React.createContext();
 
@@ -20,12 +26,22 @@ const AppProvider = ({ children }) => {
 
   const fetchStories = async (url) => {
     dispatch({ type: SET_LOADING });
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      console.log(data)
+      dispatch({ type: SET_STORIES, payload: {hits:data.hits, nbPages: data.nbPages} })
+    }
+    catch (error) {
+      console.log(error)
+    }
+
   };
 
   useEffect(() => {
-    fetchStories();
+    fetchStories(`${API_ENDPOINT}query=${state.query}&page=${state.page}`);
   }, []);
-  return <AppContext.Provider value={{...state}}>{children}</AppContext.Provider>;
+  return <AppContext.Provider value={{ ...state }}>{children}</AppContext.Provider>;
 };
 // make sure use
 export const useGlobalContext = () => {
