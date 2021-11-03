9368a3734184719092494285b7488f722ecfcd0d
Ubuntu19.10 <vjolashtishi@gmail.com>
2021-11-03 14:26:49 +0100

    Data fetching

--- sample-quiz/src/context.js
+++ sample-quiz/src/context.js
@@ -23,6 +23,33 @@ const AppProvider = ({ children }) => {
     const [error, setError] = useState(false)
 
     const [isModalOpen, setIsModalOpen] = useState(false)
+
+    const fetchQuestions = async (url) => {
+        setLoading(true)
+        setWaiting(false)
+        const response = await axios(url).catch(err => console.log(err))
+        if (response) {
+            const data = response.data.results
+            if (data.length > 0) {
+                setQuestions(data)
+                setLoading(true)
+                setWaiting(false)
+                setError(false)
+            }
+            else {
+                setWaiting(true)
+                setError(true)
+            }
+        }
+        else {
+            setWaiting(true)
+        }
+    }
+
+    useEffect(() => {
+        fetchQuestions(tempUrl)
+    }, [])
+
     return <AppContext.Provider value={{
         waiting, loading, questions, index,
         correct, error, isModalOpen
